2f8fea6d47e5dde9e7fc122f4a5f4fd2dd53036f
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-06-28 13:15:35 -0300

    Finalizado

--- server.py
+++ server.py
@@ -1,152 +1,117 @@
+import codecs
+from genericpath import exists
+import os
 import socket
 import sys
+import threading
 import datetime
 
-
 ip = 'localhost'
-port = 8181
+port = 8888
+data = datetime.datetime.now()
 
+#Pegando o tempo em cache
 timeInCache = sys.argv[1]
-#----------------------------#
-# Cache-Control: public
-# Cache-Control: max-age=3153600
-# Cache-Control: must-revalidate
+#-----------------------
 
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((ip, port))
+server.listen(5)
+print(f"Escutando: {ip} \nporta: {port}")
+server_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def conexao_em_espera(conexao, addr):
+    print('Nova Conexão: ', addr)
 
 
-def thread_client(conexao):
-    print("Nova conexão: ", conexao)
-
-def addParagraph(resposta, cache,str_dominio):
-    respostaEmString = ''
-    if str_dominio == "www-net.cs.umass.edu":
-        a = "<body>"
-        respostaEmString = resposta
-        indiceDoBody = respostaEmString.find(bytes(a, encoding='utf8'))
-        novaRespostaEmString = str(respostaEmString[:indiceDoBody + 6]) + cache + str(respostaEmString[indiceDoBody + 6:])
+def post_it(salvar_cache):
+    no_cache = '\n<p style="z-index:9999; position:fixed; top:20px; left:20px;width:200px;height:100px; background-color:yellow;padding:10px; font-weight:bold;">Novo: {}</p>'.format(data)
+    if salvar_cache.find(b'html'):
+        respostaEmString = salvar_cache.decode()
+        indiceDoBody = respostaEmString.find("<body>")
+        novaRespostaEmString = respostaEmString[:indiceDoBody + 6] + no_cache + respostaEmString[indiceDoBody + 6:]
+        print(novaRespostaEmString)
+        if novaRespostaEmString.find("Cache-Control: max-age=604800"):
+            novaRespostaEmString = novaRespostaEmString.replace("Cache-Control: max-age=604800","Cache-Control: max-age=120")
+            print(novaRespostaEmString)
         return novaRespostaEmString
     else:
-        respostaEmString = resposta.decode()
-        indiceDoBody = respostaEmString.find("<body>")
-        novaRespostaEmString = respostaEmString[:indiceDoBody + 6] + cache + respostaEmString[indiceDoBody + 6:]
-        print(novaRespostaEmString)
-        return novaRespostaEmString
+        return salvar_cache
+   
+
+def salvar_em_cache(carregamento_pag, conexao, str_dominio,imag_str):
+    salvar_cache = codecs.decode(carregamento_pag, encoding= 'base64')
+    salvar_cacheSTR = str(salvar_cache)
+    arquivo = open(str_dominio+imag_str+'.txt','w')
+    arquivo.write(salvar_cacheSTR)
+    #arquivo.close()
+    #amarelonatela = 
+    #post_it(salvar_cache,conexao)
+    #return amarelonatela
+
+def ler_cache(str_dominio,imag_str):
+    ler_arquivo = open(str_dominio+imag_str,'r')
+    return ler_arquivo
+
+def conexao_browser(str_dominio, url_Complexa_Divisao, urlTratamento, conexao, addr,imag_str):
+   
+    teste_tamanho_url = (len(url_Complexa_Divisao))
+    #www.example.org
+    if teste_tamanho_url != 0:
+        for count in range (len(url_Complexa_Divisao)-1):
+            concatenar_url_complex = url_Complexa_Divisao[count]+'/'+url_Complexa_Divisao[count+1]
+            concatenar_url_complexSTR = str(concatenar_url_complex)
+            complemento_url = concatenar_url_complexSTR.split("'")[0]
+            conexao_url = ('GET /'+complemento_url+' HTTP/1.1\r\nHost: '+str_dominio+'\r\n\r\n')
+    else:
+        conexao_url = ('GET / HTTP/1.1\r\nHost: '+str_dominio+'\r\n\r\n')
         
-
-
+    server_client.sendall(conexao_url.encode())
+    carregamento_pag = server_client.recv(350000)
+    carregamento_pag_com_post_it = post_it(carregamento_pag)
+    conexao.sendall(carregamento_pag_com_post_it.encode())
+    print("Valor Informado: ", urlTratamento)
+    thread = threading.Thread(target=conexao_em_espera, args=(conexao,addr))
+    thread.start()
+    print(f"Conexão Recebida: ", {threading.active_count() - 1})
+    salvar_em_cache(carregamento_pag,conexao, str_dominio,imag_str)
 
 
 def main():
-
-    global cached, noCache, data
-    cached = ''
-    noCache = ''
-
-    arquivo = open("cache.txt","w")
-
-    contador = 1    
-    print("[*] Iniciando servidor")
-    
-    #SOCKET PRINCIPAL
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((ip,port))
-    server.listen(10)
-
-    #SOCKET CLIENT
-    server_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    print("[*] Aguardando por conexões")
-    contador = 1
     while True:
-        
-        
+        #tratamento do corpo da URL
         conexao, addr = server.accept()
-        request = conexao.recv(500000)
-
-        #PARTE DO TRATAMENTO PARA O FAVICON.ICO
-        requisicao_favicon = request.split()
+        requisicao = conexao.recv(350000) 
+        urlTratamento = requisicao.split()[1]
+        urlTratamento_STR = str(urlTratamento)
+        urlTratamento_ASPAS = urlTratamento_STR.split("'")[1]
+        urlTratamento_ASPAS_STR = str(urlTratamento_ASPAS)
+        selec_imagem  = urlTratamento_ASPAS_STR.split(".")
+        imag_str = str(selec_imagem [-1])
+        urlTratamento_BARRA = urlTratamento_ASPAS_STR.split('/')[1]
+        urlTratamento_BARRA_STR = str(urlTratamento_BARRA)
+        url_Complexa = urlTratamento_STR.split('/')
+        url_Complexa_Divisao = url_Complexa[2:]
+        str_dominio = urlTratamento_BARRA_STR.strip('')
         
-        #TRATAMENTO DE URL
-        splita_espaco = request.split()[1]
-        converte_splitespaco = str(splita_espaco)
-        splita_aspas = converte_splitespaco.split("'")[1]
-        converte_splitaspas = str(splita_aspas)
-        splita_barra = converte_splitaspas.split('/')
-        enderecopassado = converte_splitaspas.split("/")[1]
-        str_dominio = str(enderecopassado)
-        #print(str_dominio)
-        
-
-        #Verificação Favicon.ico
-        if(str_dominio == "favicon.ico"):
-            for i in requisicao_favicon:
-                if len(i) > 5:
-                    if((i[3:]).decode() == "p://localhost:7777/www.example.org"):
-                        guardavalor = (i[3:]).decode()
-                        splita_valor = guardavalor.split("/")
-                        urlfinal = splita_valor[3]
-                        str_dominio = urlfinal
-                        server_client.connect((str_dominio, 80))
+        # Tratamento Favicon e Imagem
+        if(str_dominio == 'favicon.ico'):
+            continue
         else:
             try:
-                server_client.connect((str_dominio, 80))
+                server_client.connect((str_dominio,80))
             except:
                 pass
-        
-        caminhoConcatenado = ''
-        if(len(splita_barra) == 3):
-            temp = splita_barra[2]
-            temp2 = temp + '/' + splita_barra[3]
-            server_client.send('GET /{} HTTP/1.1\r\nHost: {}\r\n\r\n'.format(temp2,str_dominio).encode()) 
-        elif(len(splita_barra) >= 4):
-            for i in range(2, len(splita_barra)):
-                if(i == len(splita_barra) - 1):
-                    caminhoConcatenado += splita_barra[i]
-                else:
-                    caminhoConcatenado += splita_barra[i] + '/'
-            server_client.send('GET /{} HTTP/1.1\r\nHost: {}\r\n\r\n'.format(caminhoConcatenado,str_dominio).encode()) 
-        else:
-            server_client.send('GET / HTTP/1.1\r\nHost: {}\r\n\r\n'.format(str_dominio).encode()) 
 
-        page = server_client.recv(500000)
-        resposta = page
-        
-        data = datetime.datetime.now()
-        print(resposta.decode())
-        arquivo.write(resposta.decode())
-        arquivo.close()
-        '''
-        try:
-            arquivo.write(resposta.decode())
-        except:
-            pass
-        '''
-        #if str_dominio == "www-net.cs.umass.edu":
-        #    str_dominio = "www-net.cs.umass.edu/personnel/kurose.html"
+        if os.path.exists(str_dominio+imag_str):
+            if str_dominio+imag_str == '_io.TextIOWrapper':
+                continue
+            else:
+                carregamento_do_browser= ler_cache(str_dominio,imag_str)
+                conexao.sendall(carregamento_do_browser)     
+        else:    
+            conexao_browser(str_dominio, url_Complexa_Divisao, urlTratamento, conexao, addr, imag_str)
 
-        if contador != 1:
-            print("está em cache")
-            cached = '\n<p style="z-index:9999; position:fixed; top:20px; left:20px;width:200px;height:100px; background-color:yellow;padding:10px; font-weight:bold;">Cache: {}</p>'.format(data)
-            novaRespostaComCache = addParagraph(resposta,cached,str_dominio)
-            conexao.sendall(novaRespostaComCache.encode())
-        else:
-            print("Primeira vez")
-            noCache = '\n<p style="z-index:9999; position:fixed; top:20px; left:20px;width:200px;height:100px; background-color:yellow;padding:10px; font-weight:bold;">Nova em: {}</p>'.format(data)
-            # "resposta" está em formato de bytes
-            novaRespostaSemCache = addParagraph(resposta,noCache,str_dominio)
-            conexao.send(novaRespostaSemCache.encode())
-        
-        thread_client(conexao)
-        #conexao.close()
-        #ou 
-        #conexao.shutdown()
-        #try:
-        #    conexao.shutdown() 
-        #except:
-        #    pass
-        
-        contador += 1 
-   
-        
-main()
+if __name__ == "__main__":
+    main()
